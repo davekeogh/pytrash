7c1712ed1e58feef6f75a2c3fa90fbaba1f16637
David Keogh <davidtkeogh@gmail.com>
2019-04-17 11:28:28 -0700

    Fix a stupid git error
    This reverts commit 728212bf416d9ae4e73df2334d52bcb4887c4ff9.

--- daemon.py
+++ daemon.py
@@ -5,7 +5,7 @@
 # This makes use of trash-empty from the trash-cli package
 
 
-import os, os.path, time, threading
+import hashlib, os, os.path, time, threading
 from configparser import ConfigParser
 
 from xdg import BaseDirectory
@@ -19,11 +19,28 @@ ICON_EMPTY = 'user-trash'
 
 EMPTY_COMMAND = 'trash-empty'
 
+DEFAULT_DESKTOP_FILE = '''[Desktop Entry]
+Version=1.1
+Type=Application
+Name=Trash
+Comment=Where the rubbish goes
+Icon=user-trash-full
+Exec=xdg-open "trash://"
+Actions=empty-trash;
+Categories=
+StartupNotify=false
+
+[Desktop Action empty-trash]
+Name=Empty Trash
+Exec=trash-empty
+'''
+
+SHA256SUM_DESKTOP_FILE = '8b9e6c091826a85cdc544fe91ed6bdd5748c719611f026ea58d70a62d0f2f3e7'
 
 def worker():
     desktop_file = ConfigParser()
     desktop_file.optionxform = str 
-    desktop_file.read(DESKTOP_FILE)
+    desktop_file.read_file(DESKTOP_FILE)
 
     items = os.listdir(TRASH_DIR + '/files')
 
@@ -41,10 +58,22 @@ def worker():
         desktop_file.write(f)
 
 
+def install_desktop_file():
+    with open(DESKTOP_FILE, 'x') as f:
+        f.write(DEFAULT_DESKTOP_FILE)
+
+
 if __name__ == '__main__':
     if not os.path.isfile(DESKTOP_FILE):
-        # TODO: Install the desktop file
-        pass
+        install_desktop_file()
+    else:
+        sha256 = hashlib.sha256()
+        with open(DESKTOP_FILE, 'rb') as f:
+            if not hashlib.sha256(f.read()) == SHA256SUM_DESKTOP_FILE:
+                # The default (empty) desktop file should always be used on startup
+                os.remove(DESKTOP_FILE)
+                install_desktop_file()
+
 
     while (True):
         t = threading.Thread(target=worker)
@@ -52,4 +81,3 @@ if __name__ == '__main__':
         t.join()
 
         time.sleep(1)
-
